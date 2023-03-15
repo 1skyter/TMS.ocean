d257166afba9c50f755b54a273ecc7dae7a9e52d
1skyter <rodiontrach78@icloud.com>
2023-03-15 12:13:26 +0100

    ocean-1
    prepared object

---
+++ diplom.ocean/diplom.ocean/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  diplom.ocean
+//
+//  Created by Rodion Trach on 15.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
